fe818cd9fae215e5192c83ed30e9991dd513b909
mykoakbar <yeikoakbar20@gmail.com>
2026-02-01 18:02:23 +0700

    add renaksi for level_2

--- src/components/pages/rencanakinerja/Table.tsx
+++ src/components/pages/rencanakinerja/Table.tsx
@@ -298,7 +298,7 @@ export const TablePerencanaan = () => {
                                                 <TbPencil className="mr-1" />
                                                 Edit Rekin
                                             </ButtonSkyBorder>
-                                            {(user?.roles == 'level_3' || user?.roles == 'level_4') &&
+                                            {(user?.roles == 'level_2' || user?.roles == 'level_3' || user?.roles == 'level_4') &&
                                                 <ButtonGreenBorder
                                                     className="w-full"
                                                     halaman_url={`/rencanakinerja/${data.id_rencana_kinerja}`}
