cbf0d6849f5e4667a472fc09dec1766f4a6ac8ab
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-07-03 10:25:35 +0700

    pakai option opd baru di master pegawai

--- src/components/pages/datamaster/masterpegawai/FormMasterPegawai.tsx
+++ src/components/pages/datamaster/masterpegawai/FormMasterPegawai.tsx
@@ -43,7 +43,7 @@ export const FormMasterPegawai = () => {
         const API_URL = process.env.NEXT_PUBLIC_API_URL;
         setIsLoading(true);
         try {
-            const response = await fetch(`${API_URL}/opd/findall`, {
+            const response = await fetch(`${API_URL}/opd/options`, {
                 method: 'GET',
                 headers: {
                     Authorization: `${token}`,
